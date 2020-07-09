69883bf74fd3f2c3a84a35764ca3a4e0ee546083
brahmam30 <68000318+brahmam30@users.noreply.github.com>
2020-07-10 00:44:11 +0530

    Add files via upload

---
+++ Day2_B23_python fundamentals.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name ='sai'
+print(name)
+
+
+# In[2]:
+
+
+name1 ="sai ram"
+print(name1)
+
+
+# In[3]:
+
+
+name2 ="""sai ram dev"""
+print(name2)
+
+
+# In[4]:
+
+
+# introduction to built in methods of a string
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+type(name)
+
+
+# In[6]:
+
+
+type(name1)
+
+
+# In[7]:
+
+
+type(name2)
+
+
+# In[8]:
+
+
+full_name  ='sai'
+print(full_name)
+
+
+# In[9]:
+
+
+print(full_name.title())
+
+
+# In[10]:
+
+
+#req to convert in to capital case
+
+
+# In[11]:
+
+
+print(full_name.upper())
+
+
+# In[ ]:
+
+
+
+
