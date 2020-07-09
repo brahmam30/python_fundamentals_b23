6acc5ff148e6ddfad4245e5e05b5f8b64308fc8c
brahmam30 <68000318+brahmam30@users.noreply.github.com>
2020-07-10 00:48:56 +0530

    Add files via upload

---
+++ Day3_B23_Python fundamentals.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first_name ='sai'
+last_name ='kumar'
+
+#general syntax of f string:
+
+
+# In[9]:
+
+
+full_name = f"{first_name}{last_name}"
+
+
+# In[10]:
+
+
+print (full_name)
+
+
+# In[12]:
+
+
+print(full_name.title())
+
+
+# In[13]:
+
+
+# keep up the good work------------custom lines
+
+
+# In[18]:
+
+
+print (f"keep up the work,{full_name.title()}")
+
+
+# In[19]:
+
+
+#adding white space to strings:
+
+
+# In[20]:
+
+
+print('fav language:pythonjavacc#')
+
+
+# In[21]:
+
+
+#improvisation of the code
+
+
+# In[26]:
+
+
+print('fav language:\npython\njava\nc\nc#')
+
+
+# In[29]:
+
+
+print('fav language:\n\tpython\n\tjava\n\tc\n\tc#')
+
+
+# 
+
+# In[30]:
+
+
+#removing whitespaces to strings
+
+
+# In[32]:
+
+
+fav_lang ='python '
+print(fav_lang)
+
+
+# In[33]:
+
+
+fav_lang.rstrip()
+
+
+# In[34]:
+
+
+fav_lang=' python'
+print(fav_lang)
+
+
+# In[35]:
+
+
+fav_lang.lstrip()
+
+
+# In[37]:
+
+
+fav_lang=' python '
+print(fav_lang)
+
+
+# In[38]:
+
+
+fav_lang.strip()
+
+
+# In[ ]:
+
+
+
+
